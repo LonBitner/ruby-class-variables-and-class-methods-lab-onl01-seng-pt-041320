e6d0b568425e7e15174c4af7058a60b3c5e68840
Lon Bitner <lonbitner@gmail.com>
2020-05-02 03:53:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,13 @@
 class Song
   attr_accessor :name, :artist, :genre
 
+  @@count = 0
   
+  def initialize(song_name, artist, genre)
+    @@count += 1
+  end
+  
+  def self.count
+    @@count
+  end
 end
